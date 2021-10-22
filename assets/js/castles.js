49721cc5c3d173b9f75ff8f5f80157f9d6cfa096
Christopher Barber <chriztoph@hotmail.com>
2021-10-21 20:21:57 -0600

    can't remember what I changed

---
+++ assets/js/castles.js
@@ -0,0 +1,17 @@
+// id - Unique identifier (number);
+// title - Image heading (string);
+// description - Image description (string: 10-25 words);
+// width - Image width in pixels (number);
+// height - Image height in pixels (number);
+// pathURL - a local path to your image file(string);
+// linkURL - 3rd party web page that the image links to (string; example: wikipedia entry relating to the image);
+// credit - The photo credit for the image such as a person, company or website (string).
+// creditURL - A link to the original photo, photographer's home page, license details, etc (string).
+const castles [
+{
+  id: 1,
+  title: castle1,
+  description: "Castle under the moonlight. The castle has red stones and spires.",
+}
+
+]
