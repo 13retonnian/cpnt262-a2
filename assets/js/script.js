2513ea584c81588c6f5caa4d50ec34af87919f1b
13retonnian <13retonnian@protonmail.com>
2021-10-22 21:47:38 -0600

    fomatted grid and made pretty more

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,10 @@ const castleOutput = function() {
   imageSection.innerHTML += `
   <figcaption>    
     <h2>${castles[i].title}</h2>
-    <a href="${castles[i].linkURL}"><h3>Picture taken by: ${castles[i].credit}</h2></a>
+    <a href="${castles[i].linkURL}"><h3>Picture taken by: ${castles[i].credit}</h3></a>
     <p>${castles[i].description}</p>
-    <a href="${castles[i].creditURL}"><img src="assets/img/castle${castles[i].id}.jpg" alt="${castles[i].title}">
+    <p>${castles[i].width}px x ${castles[i].height}px</p>
+    <a href="${castles[i].creditURL}"><img src="assets/img/castle${castles[i].id}.jpg" alt="${castles[i].title}">    
   </figcaption>`;
   i++;
 }
