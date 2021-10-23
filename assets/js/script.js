7d296d8369455cb0de28fb395a108cc8cbdfcc1d
13retonnian <13retonnian@protonmail.com>
2021-10-22 21:17:48 -0600

    fomatted grid and made pretty

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 const imageSection = document.querySelector(".image-section");
 let i = 0;
 const castleOutput = function() {  
-  imageSection.innerHTML += `<img src="assets/img/castle${castles[i].id}.jpg" alt="${castles[i].title}">`;
+  imageSection.innerHTML += `
+  <figcaption>    
+    <h2>${castles[i].title}</h2>
+    <a href="${castles[i].linkURL}"><h3>Picture taken by: ${castles[i].credit}</h2></a>
+    <p>${castles[i].description}</p>
+    <a href="${castles[i].creditURL}"><img src="assets/img/castle${castles[i].id}.jpg" alt="${castles[i].title}">
+  </figcaption>`;
   i++;
 }
 
